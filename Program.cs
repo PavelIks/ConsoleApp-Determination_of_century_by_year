9f4e41f70da929109718b8b11fa4c33be02589b6
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-08-05 15:27:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CenturyYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите год, чтобы узнать век: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{(year - 1) / 100 + 1} век");
+            Console.ReadKey();
+        }
+    }
+}
